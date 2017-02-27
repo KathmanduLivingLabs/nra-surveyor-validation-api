d193ef5a5b2845464fe2dff5aa7f44504ef537f5
drklrd <srvbhattarai@gmail.com>
2017-02-27 19:09:27 +0545

    change sequelize conenction for heroku

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ var path      = require("path");
 var config = require ('../config');
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(config.db.database,config.db.username,config.db.password,{
-    dialect : config.db.dialect
-});
+// var sequelize = new Sequelize(config.db.database,config.db.username,config.db.password,{
+//     dialect : config.db.dialect
+// });
 
-// var sequelize =  new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres'
-// }); // for heroku deployment 
+var sequelize =  new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+}); // for heroku deployment 
 
 
 var db        = {};

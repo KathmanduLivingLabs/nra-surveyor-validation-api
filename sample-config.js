5f46227c0072dbfe13284dac6e3a47074013f15b
drklrd <srvbhattarai@gmail.com>
2017-02-27 18:22:01 +0545

    update sample-config.js

--- sample-config.js
+++ sample-config.js
@@ -1,5 +1,14 @@
 module.exports = {
 	versions: {
 		v1: '/api/v1'
+	},
+	db : {
+	    username : 'nraadmin',
+	    password : 'nraadmin!@#',
+	    database : 'nrasurveyors',
+	    host : '127.0.0.1',
+	    dialect : 'postgres',
+	    port : 3306
+
 	}
 }
